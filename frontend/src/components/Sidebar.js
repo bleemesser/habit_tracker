8a91dbf59d526d21ff64148d7fb56658786b44ff
Russell Ross <rusross@nuevaschool.org>
2022-11-20 21:13:08 -0800

    Refined code

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -39,75 +39,61 @@ class Sidebar extends React.Component { // simple sidebar to display the most ba
             }
         }
         total = pcount + scount + fcount;
-        return (
-            <table className='table table-sm table-borderless' id='statstable'>
-                <tbody>
-                    <tr>
-                        <th>Procrastination</th>
-                        <td>{pcount}</td>
-                    </tr>
-                    <tr>
-                        <th>Sleep</th>
-                        <td>{scount}</td>
-                    </tr>
-                    <tr>
-                        <th>Feelings</th>
-                        <td>{fcount}</td>
-                    </tr>
-                    <tr>
-                        <th>Total</th>
-                        <td>{total}</td>
-                    </tr>
-                    <tr>
-                        <th>Last Submission</th>
-                        <td>{lasteventdate}</td>
-                    </tr>
-                </tbody>
-            </table>
-        );
-    }
 
-    // Get data for chart
-    getData() {
-        let procrastinationCount = 100;
-        let sleepCount = 120;
-        let feelingsCount = 300;
-        for (let i = 0; i < this.props.events.length; i++) {
-            if (this.props.events[i].type === "procrastination") {
-                procrastinationCount++;
-            }
-            else if (this.props.events[i].type === "sleep") {
-                sleepCount++;
-            }
-            else if (this.props.events[i].type === "feelings") {
-                feelingsCount++;
-            }
-        }
-        const data =  {
-            labels: ['Procrastination', 'Sleep', 'Feelings'],
-            datasets: [
-                {
-                    label: 'Number of Events',
-                    data: [procrastinationCount, sleepCount, feelingsCount],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)', // red (procrastination)
-                        'rgba(54, 162, 235, 0.2)', // blue (sleep)
-                        'rgba(255, 206, 86, 0.2)'  // yellow (feelings)
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)', 
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)'
-                    ],
-                    borderWidth: 1,
-                },
+        const data = {
+            labels: [
+                'Procrastination',
+                'Sleep',
+                'Feelings'
             ],
+            datasets: [{
+                label: 'Number of Events',
+                data: [pcount, scount, fcount],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)', //red: procrastination
+                    'rgba(54, 162, 235, 0.2)', //blue: sleep
+                    'rgba(255, 206, 86, 0.2)'  //yellow: feelings
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1,
+            }],
         };
+
         return (
-            <Doughnut data={data} />
+            <div className="Data">
+                <table className='table table-sm table-borderless' id='statstable'>
+                    <tbody>
+                        <tr>
+                            <th className="text-[#ff6384]">Procrastination</th>
+                            <td>{pcount}</td>
+                        </tr>
+                        <tr>
+                            <th className="text-[#36a3eb]">Sleep</th>
+                            <td>{scount}</td>
+                        </tr>
+                        <tr>
+                            <th className="text-[#ffcf56]">Feelings</th>
+                            <td>{fcount}</td>
+                        </tr>
+                        <tr>
+                            <th>Total</th>
+                            <td>{total}</td>
+                        </tr>
+                        <tr>
+                            <th>Last Submission</th>
+                            <td>{lasteventdate}</td>
+                        </tr>
+                        <button className='btn btn-light' onClick={this.exportEvents}>Download Data</button>
+                    </tbody>
+                </table>
+                <Doughnut data={data} />
+            </div>
         );
     }
-
 
     //allow the student to download a csv of their form history
     exportEvents() {
@@ -138,13 +124,10 @@ class Sidebar extends React.Component { // simple sidebar to display the most ba
     }
 
     render() {
-        return (
-            <aside className='border py-4 px-2 w-64 flex flex-col justify-top items-center'>
+        return ( // render the sidebar
+            <aside className='border py-4 px-2 w-64 p-100'>
                 <h3 className='text-center text-lg'>Logs:</h3>
-               {this.unpackEvents()}
-               <button className='btn btn-light' onClick={this.exportEvents}>Download Data</button>
-                <h3 className='text-center text-lg'>Data Visualization:</h3>
-                {this.getData()}'
+                {this.unpackEvents()}
             </aside>
         )
     }
